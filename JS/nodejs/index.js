35a47e1df71055621ff3944f2398a6d47c6d7d16
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-06-09 23:39:42 +0900

    today's study review and I use express

---
+++ JS/nodejs/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var router = require('./router/main')(app);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+var server = app.listen(3000, function(){
+    console.log("Express server has started on port 3000")
+});
+
