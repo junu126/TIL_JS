a9c25ff6d8d97aec67d5c83194ff318aeb434329
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-09-10 20:28:03 +0900

    폴더 옮김

--- python/입출력문제.py
+++ python/입출력문제.py
@@ -1,25 +1,25 @@
 # 파일 읽고 출력하기
 
-with open("./test.txt", 'w') as f1:
+with open("./FileInputOutput/test.txt", 'w') as f1:
     f1.write("Life is too short")
 
-with open("./test.txt", 'r') as f2:
+with open("./FileInputOutput/test.txt", 'r') as f2:
     print(f2.read())
 
 # 파일저장
 
-with open('./test2.txt', 'a') as f3:
+with open('./FileInputOutput/test2.txt', 'a') as f3:
     _input = input()
     f3.write("{0}\n".format(_input))
 
 # 역순 저장
 
-with open('./abc.txt', 'r') as f4:
+with open('./FileInputOutput/abc.txt', 'r') as f4:
     txt = f4.readlines()
 
 _txt = reversed(txt)
 
-with open('./abc.txt', 'w') as f4:
+with open('./FileInputOutput/abc.txt', 'w') as f4:
     for i in _txt:
         i = i.strip()
         f4.write(i)
@@ -27,7 +27,7 @@ with open('./abc.txt', 'w') as f4:
 
 # 파일 수정
 
-with open('./text.txt', 'r') as f5:
+with open('./FileInputOutput/text.txt', 'r') as f5:
     reading = f5.read()
 creading = reading.replace('java','python')
 with open('./text.txt', 'w') as f5:
@@ -35,7 +35,7 @@ with open('./text.txt', 'w') as f5:
 
 # 평균값 구하기
 
-with open('./sample.txt', 'r') as f6:
+with open('./FileInputOutput/sample.txt', 'r') as f6:
     num = f6.readlines()
 
 _sum = 0
@@ -43,5 +43,5 @@ for i in num:
     _sum += int(i)
 aver = _sum / len(num)
 
-with open('./sample.txt', 'a') as f6:
+with open('./FileInputOutput/sample.txt', 'a') as f6:
     f6.write('\n\n 총합 : {0}\n평균 : {1}'.format(_sum, aver))
