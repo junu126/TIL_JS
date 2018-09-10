8fd9b11df1382c9664c307dd4e972b772c76fb67
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-09-10 14:35:40 +0900

    for문 문제풀이

---
+++ python/for문.py
@@ -0,0 +1,47 @@
+# 1번 문제
+
+#for a in range(1,101):
+#    print(a)
+
+# 2번 문제
+
+#_sum = 0
+#for i in range(1,1001):
+#    if i % 5 == 0:
+#        _sum = i + _sum
+#print(_sum)
+
+# 3번 문제
+
+#_sum = 0
+#A = [70, 60, 55, 75, 95, 90, 80, 80, 85, 100]
+#for i in A:
+#    _sum = _sum + i
+#ave = _sum / 10
+#print(ave)
+
+# 4번 문제
+
+
+#bloodType = ['A', 'B', 'A', 'O', 'AB', 'AB', 'O', 'A', 'B', 'O', 'B', 'AB']
+#result = {}
+
+#for i in bloodType:
+#    if i in result:
+#        result[i] += 1
+#    else:
+#        result[i] = 1
+#print(result)
+
+# 5번 문제
+
+#numbers = [1,2,3,4,5]
+#result = [n * 2 for n in numbers if n% 2 == 1]
+#print(result)
+
+# 6번 문제
+
+#que = 'Life is too short, you need python'
+#ans = 'aeiou'
+#a=''.join( [i for i in que if i not in ans] )
+#print(a)
