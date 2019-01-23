c2e43501c487b14940bf9bf43e4fd4e422a78ee7
Kim Jun Woo <junukim.dev@gmail.com>
2019-01-23 15:06:03 +0900

    CREATE : [js] 파일경로

---
+++ JS/NodeJS/01_node_Function/filename.js
@@ -0,0 +1,10 @@
+/*
+ 노드 실행 경로
+ 터미널 -> cd 01_node_Function -> node filename
+*/
+
+// 파일의 경로
+console.log(__filename);  // C:\Users\DSM\Documents\GitHub\TIL_JS\JS\NodeJS\01_node_Function\filename.js
+
+// 파일이 위치한 폴더의 경로
+console.log(__dirname); // C:\Users\DSM\Documents\GitHub\TIL_JS\JS\NodeJS\01_node_Function
