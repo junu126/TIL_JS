97b98704f574b03c40b95c32fe6aeb8f9595f032
Kim Jun Woo <junukim.dev@gmail.com>
2019-01-29 23:51:51 +0900

    CREATE : ExpressGnarator

---
+++ JS/NodeJS/05_node_express/learn-express/app.js
@@ -0,0 +1,57 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');
+var flash = require('connect-flash'); // 일회성 메시지 기능.
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(function(req, res, next) {
+  console.log(req.url, '저도 미들웨어 입니다.');
+  next();
+});
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser('secret code'));
+app.use(session({
+  resave: false,  // 요청을 받았을 때 수정사항이 없어도 세션을 다시 저장할까요?
+  saveUninitialized: false, // 세션에 저장할 내용이 없더라도 세션을 저장할까요?
+  secret: 'secret code',
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+}));
+app.use(flash());
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
