55fa3e055608703b1d4dd93c52fadf50a1f9f781
Kim Jun Woo <junukim.dev@gmail.com>
2019-02-08 23:10:26 +0900

    CREATE : add req.session method

---
+++ JS/NodeJS/08_node_snsService/nodebird/passport/index.js
@@ -0,0 +1,18 @@
+const local = require('./localStrategy');
+const kakao = require('./kakaoStrategy');
+const { User } = require('../models');
+
+module.exports = (passport) => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);  // done('에러 발생 시', '세션에 등록할 데이터');
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.find({ where: { id } })
+      .then(user => done(null, user))
+      .catch(err => done(err));
+  });
+
+  local(passport);
+  kakao(passport);
+}
