2b5cba5cded9e48a62803e4fa369f33bddc37760
Kim Jun Woo <junukim.dev@gmail.com>
2019-02-11 22:39:19 +0900

    CREATE : login service

---
+++ JS/NodeJS/09_node_snsService-API/nodebird-api/passport/index.js
@@ -0,0 +1,29 @@
+const local = require('./localStrategy');
+const kakao = require('./kakaoStrategy');
+const { User } = require('../models');
+
+module.exports = (passport) => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);  // done('에러 발생 시', '세션에 등록할 데이터');
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.find({ 
+      where: { id },
+      include: [{
+        model: User,
+        attributes: ['id', 'nick'],
+        as: 'Followers',
+      }, {
+        model: User,
+        attributes: ['id', 'nick'],
+        as: 'Followings',
+      }],
+    })
+      .then(user => done(null, user))
+      .catch(err => done(err));
+  });
+
+  local(passport);
+  kakao(passport);
+}
