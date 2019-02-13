a805f593369a4967e40481a3180a10787d051e00
Kim Jun Woo <junukim.dev@gmail.com>
2019-02-13 23:44:08 +0900

    UPDATE : CORS

--- JS/NodeJS/09_node_snsService-API/nodebird-api/routes/v2.js
+++ JS/NodeJS/09_node_snsService-API/nodebird-api/routes/v2.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const cors = require('cors');
+const url = require('url');
 
 const { verifyToken, apiLimiter } = require("./middlewares");
 const { Domain, User, Post, Hashtag } = require("../models");
 
 const router = express.Router();
 
+router.use(async (req, res, next) => {
+  const domain = await Domain.find({
+    where: { host: url.parse(req.get('origin')).host },
+  });
+  if (domain) {
+    cors({ origin: req.get('origin') })(req, res, next);
+  } else {
+    next();
+  }
+});
 
 router.post("/token", apiLimiter, async (req, res) => {
   const { clientSecret } = req.body;
-
+  
   try {
     const domain = await Domain.find({
       where: { clientSecret },
@@ -44,7 +56,7 @@ router.post("/token", apiLimiter, async (req, res) => {
     return res.json({
       code: 200,
       message: "토큰이 발급되었습니다.",
-      token
+      token,
     });
   } catch (error) {
     console.error(error);
