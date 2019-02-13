11af8851fd27c16f8a3367645f3e122cad7c7d3c
Kim Jun Woo <junukim.dev@gmail.com>
2019-02-13 23:56:28 +0900

    CREAET : apidocs

---
+++ JS/NodeJS/09_node_snsService-API/nodebird-api/apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "nodbird apidocs",
+  "version": "0.0.1",
+  "description": "nodebird apidocs",
+  "verson": "0.0.1",
+  "title": "My First Apidocs",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-02-13T14:51:51.572Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
--- JS/NodeJS/09_node_snsService-API/nodebird-api/app.js
+++ JS/NodeJS/09_node_snsService-API/nodebird-api/app.js
@@ -1,3 +1,14 @@
+/**
+ * @api {get} /user/:id Request User information
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ */
+
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
