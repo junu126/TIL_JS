f385bfc0e3e166d5e1533caeb18372c860876e05
Kim Jun Woo <junukim.dev@gmail.com>
2019-02-13 23:44:25 +0900

    UPDATE : CORS

--- JS/NodeJS/09_node_snsService-API/nodebird-call/routes/index.js
+++ JS/NodeJS/09_node_snsService-API/nodebird-call/routes/index.js
@@ -46,4 +46,8 @@ router.get('/search/:hashtag', async (req, res, next) => {
   }
 })
 
+router.get('/', (req, res) => {
+  res.render('main', { key: process.env.CLIENT_SECRET });
+});
+
 module.exports = router;
