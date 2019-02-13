0e531ef15e00515b9b782b88e4f924ed7d0948de
Kim Jun Woo <junukim.dev@gmail.com>
2019-02-13 22:42:40 +0900

    UPDATE : limiting

--- JS/NodeJS/09_node_snsService-API/nodebird-call/routes/index.js
+++ JS/NodeJS/09_node_snsService-API/nodebird-call/routes/index.js
@@ -2,31 +2,48 @@ const express = require('express');
 const axios = require('axios');
 
 const router = express.Router();
+const URL = 'http://localhost:8002/v2';
 
-router.get('/test', async (req, res, next) => {
+const request = async (req, api) => {
   try {
     if (!req.session.jwt) {
-      const tokenResult = await axios.post('http://localhost:8002/v1/token', {
+      const tokenResult = await axios.post(`${URL}/token`, {
         clientSecret: process.env.CLIENT_SECRET,
       });
-      if (tokenResult.data && tokenResult.data.code === 200) {
-        req.session.jwt = tokenResult.data.token;
-      } else {
-        return res.json(tokenResult.data);
-      }
+      req.session.jwt = tokenResult.data.token;
     }
-
-    const result = await axios.get('http://localhost:8002/v1/test', {
+    return await axios.get(`${URL}${api}`, {
       headers: { authorization: req.session.jwt },
     });
-    return res.json(result.data);
   } catch (error) {
     console.error(error);
-    if (error.response.status === 419) {
-      return res.json(error.response.data);
+    if (error.response.status < 500) {
+      return error.response;
     }
-    return next(error);
+    throw error;
   }
-});
+};
+
+router.get('/mypost', async (req, res, next) => {
+  try {
+    const result = await request(req, '/posts/my');
+    res.json(result.data);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+})
+
+router.get('/search/:hashtag', async (req, res, next) => {
+  try {
+    const result = await request(req, `/posts/hashtag/${encodeURIComponent(req.params.hashtag)}`);
+    res.json(result.data);
+  } catch (error) {
+    if (error.code) {
+      console.error(error);
+      next(error);
+    }
+  }
+})
 
 module.exports = router;
