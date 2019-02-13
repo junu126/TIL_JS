e9d041f1f582f95c6d8c06beb0155701e34e63a3
Kim Jun Woo <junukim.dev@gmail.com>
2019-02-13 22:41:47 +0900

    Update app.js

--- JS/NodeJS/09_node_snsService-API/nodebird-api/app.js
+++ JS/NodeJS/09_node_snsService-API/nodebird-api/app.js
@@ -13,6 +13,7 @@ const passportConfig = require('./passport');
 const authRouter = require('./routes/auth');
 const indexRouter = require('./routes');
 const v1 = require('./routes/v1');
+const v2 = require('./routes/v2');
 
 const app = express();
 sequelize.sync();
@@ -41,6 +42,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/v1', v1);
+app.use('/v2', v2);
 app.use('/auth', authRouter);
 app.use('/', indexRouter);
 
